68dbb23493d4d7159103bc2f1b8e02dd2f9e36f4
Alexander Kalabin <k414bin@yandex.ru>
2020-12-08 21:58:27 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Как Вас зовут?");
+			string name = Console.ReadLine();
+			Console.Write($"Здравствуйте, {name} ! Сегодня  ");
+			Console.WriteLine($"{DateTime.Today}");
+			Console.ReadLine();
+		}
+	}
+}
